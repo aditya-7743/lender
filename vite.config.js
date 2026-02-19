98983fba062d4209f0a0a8a2f9af23b8cbdd6453
aditya-7743 <adityakr.bihar@gmail.com>
2026-02-19 20:39:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lender/', // Change this to your GitHub repo name
+  base: '/', // Change this to your GitHub repo name
 })
